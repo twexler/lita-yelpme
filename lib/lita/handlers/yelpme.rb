f052196e6706488090af923098c5f4e32b7f4419
Ted Wexler <ted.wexler@xoom.com>
2014-05-07 15:49:47 -0700

    I know this is broken...

--- lib/lita/handlers/yelpme.rb
+++ lib/lita/handlers/yelpme.rb
@@ -6,23 +6,23 @@ module Lita
   	route(/^(?:yelp|y)\s+(.+)/u, :yelp, command: true, help: {
       "yelp QUERY" => "Return the first Yelp result with information about the first business found"
     })
+    @client = Yelp::Client.new({ consumer_key: Lita.config.handlers.yelpme.consumer_key,
+                                consumer_secret: Lita.config.handlers.yelpme.consumer_secret,
+                                token: Lita.config.handlers.yelpme.token,
+                                token_secret: Lita.config.handlers.yelpme.token_secret
+                             })
     def self.default_config(handler_config)
-	handler_config.consumer_key = nil
-	handler_config.consumer_secret = nil
-	handler_config.token = nil
-	handler_config.token_secret = nil
-	handler_config.default_city = "San Francisco"
+      handler_config.consumer_key = ""
+      handler_config.consumer_secret = ""
+	  handler_config.token = ""
+	  handler_config.token_secret = ""
+	  handler_config.default_city = "San Francisco"
     end
     def yelp(response)
       query = response.matches[0][0]
-      client = Yelp::Client.new({ consumer_key: config.yelpme.consumer_key,
-               consumer_secret: config.yelpme.consumer_secret,
-               token: config.yelpme.token,
-               token_secret: config.yelpme.token_secret
-            })
-      yelp_response = client.search(config.default_city, { term: query, limit: 1 })
+      yelp_response = @client.search(config.default_city, { term: query, limit: 1 })
       if len(yelp_response.businesses) == 0
-	response.reply('Cannot find any businesses in #{config.default_city} matching #{query}')
+	    response.reply('Cannot find any businesses in #{config.default_city} matching #{query}')
       end
       result = yelp_response.businesses[0]
       address = "#{result.location.address.join(' ')}, #{result.location.city}, #{result.location.state_code} #{result.location.postal_code}"
